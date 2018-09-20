603c704da95da64293d3950dd2d10afad3cc5c10
山口輝明 <taiko2010_yteruaki@yahoo.co.jp>
2018-09-20 14:36:46 +0900

    game.cpp&hの追加

---
+++ game.cpp
@@ -0,0 +1,102 @@
+//=============================================================================
+//
+// ゲーム画面処理 [game.cpp]
+//
+//=============================================================================
+#include "game.h"
+#include "Library/Input.h"
+#include "Library/Fade.h"
+#include "Library/ObjectBase2D.h"
+
+
+//ワークのインクルード
+#include "workHagiwara.h"
+#include "workKimura.h"
+#include "workSato.h"
+#include "workSon.h"
+#include "workTsu.h"
+#include "workUtsugi.h"
+#include "workYamaguchi.h"
+#include "workYamamoto.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT InitGame(void)
+{
+	InitWorkHagiwara();
+	InitWorkKimura();
+	InitWorkSato();
+	InitWorkSon();
+	InitWorkTsu();
+	InitWorkUtsugi();
+	InitWorkYamaguchi();
+	InitWorkYamamoto();
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void UninitGame(void)
+{
+	UninitWorkHagiwara();
+	UninitWorkKimura();
+	UninitWorkSato();
+	UninitWorkSon();
+	UninitWorkTsu();
+	UninitWorkUtsugi();
+	UninitWorkYamaguchi();
+	UninitWorkYamamoto();
+
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void UpdateGame(void)
+{
+	UpdateWorkHagiwara();
+	UpdateWorkKimura();
+	UpdateWorkSato();
+	UpdateWorkSon();
+	UpdateWorkTsu();
+	UpdateWorkUtsugi();
+	UpdateWorkYamaguchi();
+	UpdateWorkYamamoto();
+
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void DrawGame(void)
+{
+	DrawWorkHagiwara();
+	DrawWorkKimura();
+	DrawWorkSato();
+	DrawWorkSon();
+	DrawWorkTsu();
+	DrawWorkUtsugi();
+	DrawWorkYamaguchi();
+	DrawWorkYamamoto();
+
+}
+
+
