8fe15dcae63d515ee39bec928cd85b083fe5648a
k-takumi1217 <muuta706>
2018-10-03 16:34:32 +0900

    enemy（ＮＰＣ）を３体表示

---
+++ ローカル/game.cpp
@@ -0,0 +1,117 @@
+//=============================================================================
+//
+// ゲーム画面処理 [Game.cpp]
+//
+//=============================================================================
+#include "Game.h"
+#include "Library/Input.h"
+#include "Library/Fade.h"
+#include "Library/ObjectBase2D.h"
+#include "Library/ObjectBase3D.h"
+
+
+//ワークのインクルード
+#include "workHagiwara.h"
+#include "workKimura.h"
+#include "workSato.h"
+#include "workSon.h"
+#include "workTsu.h"
+#include "workUtsugi.h"
+#include "workYamaguchi.h"
+#include "workYamamoto.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+C2DObject        TestPlayer2D;
+C3DPolygonObject TestPlayer3D;
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT InitGame(void)
+{
+	TestPlayer2D.Init(100, 100, 50, 50, "data/作業/テスト画像01.jpg");
+
+	TestPlayer3D.Init(Vector3(200, 0, 200), Vector2(50, 50));
+	TestPlayer3D.LoadTexture("data/作業/テスト画像02.jpg");
+
+	InitWorkHagiwara();
+	InitWorkKimura();
+	InitWorkSato();
+	InitWorkSon();
+	InitWorkTsu();
+	InitWorkUtsugi();
+	InitWorkYamaguchi();
+	InitWorkYamamoto();
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void UninitGame(void)
+{
+	TestPlayer2D.Release();
+
+	TestPlayer3D.Release();
+
+	UninitWorkHagiwara();
+	UninitWorkKimura();
+	UninitWorkSato();
+	UninitWorkSon();
+	UninitWorkTsu();
+	UninitWorkUtsugi();
+	UninitWorkYamaguchi();
+	UninitWorkYamamoto();
+
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void UpdateGame(void)
+{
+	UpdateWorkHagiwara();
+	UpdateWorkKimura();
+	UpdateWorkSato();
+	UpdateWorkSon();
+	UpdateWorkTsu();
+	UpdateWorkUtsugi();
+	UpdateWorkYamaguchi();
+	UpdateWorkYamamoto();
+
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void DrawGame(void)
+{
+	TestPlayer2D.Draw();
+
+	TestPlayer3D.Draw();
+
+	DrawWorkHagiwara();
+	DrawWorkKimura();
+	DrawWorkSato();
+	DrawWorkSon();
+	DrawWorkTsu();
+	DrawWorkUtsugi();
+	DrawWorkYamaguchi();
+	DrawWorkYamamoto();
+
+}
+
+
---
+++ ローカル/workHagiwara.cpp
@@ -0,0 +1,115 @@
+//=============================================================================
+//
+//萩原担当分　workHagiwara.cpp
+//
+//=============================================================================
+
+//必須インクルード
+#include <windows.h>
+#include "workHagiwara.h"
+#include "SceneManager.h"
+
+
+//萩原担当分インクルード
+
+
+//=============================================================================
+//初期化処理
+//=============================================================================
+HRESULT InitWorkHagiwara(void)
+{
+	switch (Scene::SetScene(SCENE_MAX))
+	{
+	case SCENE_TITLE://タイトルで使いたいソースのInit
+
+		break;
+
+
+	case SCENE_GAME://ゲームで使いたいソースのInit
+
+		break;
+
+
+	case SCENE_RESULT://リザルトで使いたいソースのInit
+
+		break;
+	}
+
+	return S_OK;
+}
+
+//=============================================================================
+//終了処理　※記載場所注意
+//=============================================================================
+void UninitWorkHagiwara(void)
+{
+
+	switch (Scene::SetScene(SCENE_MAX))
+	{
+	case SCENE_TITLE://リザルトで使ったソースのUninit
+
+		break;
+
+
+	case SCENE_GAME://タイトルで使ったソースのUninit
+
+		break;
+
+
+	case SCENE_RESULT://ゲームで使ったソースのUninit
+
+		break;
+	}
+
+
+}
+
+//=============================================================================
+//更新処理
+//=============================================================================
+void UpdateWorkHagiwara(void)
+{
+	switch (Scene::SetScene(SCENE_MAX))
+	{
+	case SCENE_TITLE://タイトルで使うソースのUpdate
+
+		break;
+
+
+	case SCENE_GAME://ゲームで使うソースのUpdate
+
+		break;
+
+
+	case SCENE_RESULT://リザルトで使うソースのUpdate
+
+		break;
+	}
+
+
+}
+
+//=============================================================================
+//描画処理
+//=============================================================================
+void DrawWorkHagiwara(void)
+{
+	switch (Scene::SetScene(SCENE_MAX))
+	{
+	case SCENE_TITLE://タイトルで使うソースのDraw
+
+		break;
+
+
+	case SCENE_GAME://ゲームで使うソースのDraw
+
+		break;
+
+
+	case SCENE_RESULT://リザルトで使うソースのDraw
+
+		break;
+	}
+
+
+}
