f98ce79682884ca1c06f2b9af9fce829954713a1
necroarmoralma <necroarmoralma@gmail.com>
2018-10-24 14:57:38 +0900

    10/24作業分

--- workYamamoto.h
+++ workYamamoto.h
@@ -6,7 +6,20 @@
 #ifndef _WORKYAMAMOTO_H_
 #define _WORKYAMAMOTO_H_
 
+#define NAMEMAX	(5)
+/*****************************************************************************
+どさいどん
+*******************************************************************************/
 
+//class Rankdata
+typedef struct
+{
+	char name[256];	// ユーザー名
+	int score;		// スコア
+	int namechar[NAMEMAX];
+	int cursole_X;
+	int cursole_Y;
+}RANKDATA;
 
 
 //=============================================================================
@@ -16,5 +29,29 @@ HRESULT InitWorkYamamoto(void);
 void UninitWorkYamamoto(void);
 void UpdateWorkYamamoto(void);
 void DrawWorkYamamoto(void);
+void Initname(void);
+
+void pinokkimon(void);
+int jyureimon(void);
+
+// キーボード(真顔)
+HRESULT InitInput(HINSTANCE hInst, HWND hWnd);
+void UninitInput(void);
+void UpdateInput(void);
+
+bool GetKeyboardPress(int nKey);
+bool GetKeyboardTrigger(int nKey);
+bool GetKeyboardRepeat(int nKey);
+bool GetKeyboardRelease(int nKey);
 
 #endif
+
+
+//**********************************************
+// 連絡
+//*********************************************
+/*
+ファイル形式　csv
+データ記述　name score
+1行ごとよみこみ
+*/
