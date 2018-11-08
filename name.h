859f06b0d901776a8e74b8059b3913a2fbf4e1c2
necroarmoralma <necroarmoralma@gmail.com>
2018-11-08 12:56:03 +0900

    11/08更新分です キーボード入力に対応(仮)　文字入力はカーソルの現在値を2桁の整数で返してその値を描画するようにー テクスチャの表示がまだです

--- name.h
+++ name.h
@@ -5,7 +5,22 @@
 #ifndef _NAME_H_
 #define _NAME_H_
 
+#include "Library/Input.h"
+// でふぁいん
 #define NAMEMAX	(5)
+// もじばんだ
+#define MOJIBAN_TEX		("data/TEXTURE/mojiban.png")
+#define MOJIBAN_POS_X	(0.0)
+#define MOJIBAN_POS_Y	(0.0)
+#define MOJIBAN_WIDTH	(50.0)
+#define MOJIBAN_HEIGHT	(100.0)
+// カーソル
+#define CURSOLE_TEX		("data/TEXTURE/カーソル")
+#define CURSOLE_POS_X	(0.0)
+#define CURSOLE_POS_Y	(0.0)
+#define CURSOLE_WIDTH	(10.0)
+#define CURSOLE_HEIGHT	(10.0)
+
 /*****************************************************************************
 どさいどん
 *******************************************************************************/
