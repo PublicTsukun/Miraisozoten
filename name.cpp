80039f853a6a7561eb128148d943d19e54a9e492
necroarmoralma <necroarmoralma@gmail.com>
2018-11-14 03:52:43 +0900

    文字盤表示　カーソル表示＆移動

--- name.cpp
+++ name.cpp
@@ -6,7 +6,8 @@
 //山本担当分インクルード
 #include "name.h"
 #include <stdio.h>
-
+#include "saveranking.h"
+#include "score.h"
 // ライブラリ
 #include "Library/ObjectBase2D.h"
 #include "Library/Common.h"
@@ -14,13 +15,16 @@
 #include "Library/DebugProcess.h"
 
 
+//*********************************************************
+// Define
+//*********************************************************
+
 // 文字盤
 #define MOJI_Y_MAX		(5)		// 5行？
 #define MOJI_X_MAX		(10)	// 10列？
 
 // 仮の数字
 #define SPACE_ENTRY		(710)	// スペース入力用
-
 #define EMPTY_NO		(99)	// くうはく(初期値)
 
 // 文字
@@ -34,6 +38,45 @@
 // 伝達用
 #define KEY_IRETEKUDASAI	(777)
 
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 文字盤の表示位置を元にカーソル位置を決めています。
+// カーソルの値は基本的に変更しないでください
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// もじばん
+#define MOJIBAN_TEX		("data/作業/文字ボード.jpg")
+#define MOJIBAN_WIDTH	(300.0)
+#define MOJIBAN_HEIGHT	(300.0)
+#define MOJIBAN_POS_X	(SCREEN_CENTER_X)						// CENTER_X指定で自動的に真ん中に表示してくれる(Draw?)
+#define MOJIBAN_POS_Y	(SCREEN_CENTER_Y)
+// 文字盤の最大マス目
+#define MOJIBAN_MASUMAX_X	(9)
+#define MOJIBAN_MASUMAX_Y	(9)
+
+// もじばん２(選択された文字を出すときに使うやつ）
+#define SENTAKUMOJI_TEX		("data/作業/文字ボード.jpg")
+#define SENTAKUMOJI_WIDTH	(70.0)
+#define SENTAKUMOJI_HEIGHT	(70.0)
+#define SENTAKUMOJI_POS_X	(100.0f)//(SCREEN_CENTER_X)						// CENTER_X指定で自動的に真ん中に表示してくれる(Draw?)
+#define SENTAKUMOJI_POS_Y	(100.0f) //(SCREEN_CENTER_Y)
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 以下の値は弄らない方が…
+#define CURSOLE_TEX		("data/テスト用画像/テスト用画像.png")
+#define CURSOLE_SPACE	(2)					// 次のマスに移動するためにカーソルのX,Yにかける値
+#define CURSOLE_WIDTH	(MOJIBAN_WIDTH/10)			// 文字盤が10文字×10文字なので
+#define CURSOLE_HEIGHT	(MOJIBAN_HEIGHT/10)
+// 「あ」を初期位置に
+#define CURSOLE_POS_X	(SCREEN_CENTER_X-CURSOLE_WIDTH - (CURSOLE_WIDTH*CURSOLE_SPACE*4))
+#define CURSOLE_POS_Y	(SCREEN_CENTER_Y-CURSOLE_HEIGHT - (CURSOLE_WIDTH*CURSOLE_SPACE*4))
+
+
+
+
+
 //*****************************************************************************
 // ぐろーばるへんすう
 //*****************************************************************************
@@ -44,17 +87,18 @@ RANKDATA	rankdatawk[1];
 C2DObject cursole;
 C2DObject mojiban;
 C2DObject moji[CHAR_MAX];
+C2DObject select_moji[CHAR_MAX];		// ばばーんとだすようのやつだ
 
-// 文字の画像テクスチャ(ヘッダーに移す？？？）
+// 文字の画像テクスチャ(文字の画像を1枚ずつ用意する場合、使わないなら決しておk）
 char *Moji_FileName[]=
 {
 	"data/TEXTURE/01.png",
 };
 //*************************************************************************
-// void Initname(void)
+// HRESULT Initname(void)
 // 文字入力の初期化
 //*************************************************************************
-void InitName(void)
+HRESULT InitName(void)
 {
 	RANKDATA *rankdata = &rankdatawk[0];
 	// 名前初期化
@@ -66,15 +110,15 @@ void InitName(void)
 	rankdata[0].cursole_X = 0;
 	rankdata[0].cursole_Y = 0;
 	namechar = 0;							// 入力済文字
-	// 画像の初期化？
+	// 画像の初期化
 	cursole.Init(CURSOLE_POS_X,CURSOLE_POS_Y,CURSOLE_WIDTH,CURSOLE_HEIGHT,CURSOLE_TEX);
 	mojiban.Init(MOJIBAN_POS_X, MOJIBAN_POS_Y, MOJIBAN_WIDTH, MOJIBAN_HEIGHT, MOJIBAN_TEX);
 
-	// 各文字がぞうの初期化
-	for (int i = 0; i < CHARS_MAX; i++)
+	for (int i = 0; i < CHAR_MAX; i++)
 	{
-		moji[i].Init(CURSOLE_POS_X, CURSOLE_POS_Y, CURSOLE_WIDTH, CURSOLE_HEIGHT, Moji_FileName[i]);
+		//select_moji[i].Init(SENTAKUMOJI_POS_X, SENTAKUMOJI_POS_Y, SENTAKUMOJI_WIDTH, SENTAKUMOJI_HEIGHT, SENTAKUMOJI_TEX);
 	}
+	return S_OK;
 }
 
 //**********************************************************
@@ -90,22 +134,29 @@ void UninitName(void)
 	*/
 }
 
-
+	
 //*************************************************************
 // 描画処理
 //**************************************************************
 void DrawName(void)
 {
 	RANKDATA *rankdata = &rankdatawk[0];
-	cursole.Draw();
+
 	mojiban.Draw();
+	// カーソル書き出しの前に初期化
+	cursole.Init(CURSOLE_POS_X + ((CURSOLE_WIDTH*CURSOLE_SPACE)*rankdata[0].cursole_X), CURSOLE_POS_Y+((CURSOLE_HEIGHT*CURSOLE_SPACE)*rankdata[0].cursole_Y), CURSOLE_WIDTH, CURSOLE_HEIGHT, CURSOLE_TEX);
+	cursole.Draw();
+	//mojiban.Init(MOJIBAN_POS_X, MOJIBAN_POS_Y, MOJIBAN_WIDTH, MOJIBAN_HEIGHT, MOJIBAN_TEX);
+	
 
 	// 格納文字書き出し
 	for (int namechar = 0; namechar < NAMEMAX; namechar++)
 	{
-		// 文字位置の初期化
-		//moji[i].Init()
-		moji[rankdata[0].namechar[namechar]].Draw();
+
+		// テクスチャのセット
+		//select_moji[namechar].Init(SENTAKUMOJI_POS_X + ((SENTAKUMOJI_WIDTH)*namechar), SENTAKUMOJI_POS_Y, SENTAKUMOJI_WIDTH, SENTAKUMOJI_HEIGHT, SENTAKUMOJI_TEX);
+		select_moji[namechar].SetTexture(rankdata[0].namechar[namechar], 10, 10);	//第二引数 ますめの数X 第3 ますめの数Y
+		select_moji[namechar].Draw();
 	}
 }
 
@@ -130,134 +181,77 @@ void Update_Name(void)
 {
 	RANKDATA *rankdata = &rankdatawk[0];
 
-	int namechar = 0;	// 現在の入力文字の初期化
-						// 文字
-	int playername = 0;
-
-	//　文字入力処理
-	while (1)
+	// カーソル移動
+	if (GetKeyboardTrigger(DIK_W))
 	{
-		// 現在の入力文字数が最大でないか
-		if (namechar < NAMEMAX)
+		rankdata[0].cursole_Y--;
+		if (rankdata[0].cursole_Y < 0)
 		{
-			rankdata[0].namechar[namechar] = get_char();
-			// 受け取った値が文字か確認
-			if (rankdata[0].namechar[namechar <= EMPTY_NO])
-			{
-				namechar++;
-			}
-			else
-			{
-				switch (rankdata[0].namechar[namechar])
-				{
-				case NO_ENTRY:
-					break;
-				case BACK_CHAR:
-					rankdata[0].namechar[namechar] = EMPTY_NO;
-					namechar--;
-					break;
-				default:
-					break;
-				}
-
-			}
-		}
-		else
-		{
-			int check = 0;
-			// 戻るボタンのみ受け付け
-			check = get_char();
-			if (check == BACK_CHAR)
-			{
-				rankdata[0].namechar[namechar] = EMPTY_NO;
-				namechar--;
-			}
-		}
-
-
-	}
-
-
-
-}
-
-//******************************************************************************
-// int get_char(void)
-// カーソルキーで文字を選んで選んだ文字を格納するやつ
-// ぴのっきもん
-//***************************************************************:
-int get_char(void)
-{
-	RANKDATA *rankdata = &rankdatawk[0];
-	int tarinaimono = EMPTY_NO;	// 選ばれた文字
-
-	//　キー入力を受け取ってカーソルを上下に移動
-
-	while (1)		// 決定キーが出るまで無限ループだよ
-	{
-		if (GetKeyboardPress(DIK_W))
-		{// 上方向
-			rankdata[0].cursole_Y++;
-			// チェック
-			if (rankdata[0].cursole_Y > MOJI_Y_MAX)
-			{
-				rankdata[0].cursole_Y = 0;
-			}
-		}
-		else if (GetKeyboardPress(DIK_S))
-		{	// したキーが押された
-			rankdata[0].cursole_Y--;
-			// チェック
-			if (rankdata[0].cursole_Y < 0)
-			{
-				rankdata[0].cursole_Y = MOJI_Y_MAX;
-			}
-		}
-		else if (GetKeyboardPress(DIK_D))
-		{
-			// 右キー
-			rankdata[0].cursole_X++;
-			// チェック
-			if (rankdata[0].cursole_X > MOJI_X_MAX)
-			{
-				rankdata[0].cursole_X = 0;
-			}
-		}
-		else if (GetKeyboardPress(DIK_A))
-		{
-			// 左キー
-			rankdata[0].cursole_X--;
-			// チェック
-			if (rankdata[0].cursole_X < 0)
-			{
-				rankdata[0].cursole_X = MOJI_X_MAX;
-			}
-		}
-
-
-
-		// 文字入力処理が終わる条件
-		if (GetKeyboardPress(DIK_SPACE))	// 決定ボタンが押された
-		{
-			tarinaimono = rankdata[0].cursole_X + (rankdata[0].cursole_Y * 10);
-			// 文字が空白だった
-			if (tarinaimono == SPACE_ENTRY)
-			{
-				tarinaimono = EMPTY_NO;
-			}
-			break;
-		}
-
-		// バックスペース(文字盤にあるかも）
-		else if (GetKeyboardPress(DIK_BACKSPACE))
-		{
-			tarinaimono = BACK_CHAR;
-			break;
+			rankdata[0].cursole_Y = MOJIBAN_MASUMAX_Y;
 		}
 	}
-	return tarinaimono;
-	
+	else if (GetKeyboardTrigger(DIK_S))
+	{	// したキーが押された
+		rankdata[0].cursole_Y++;
+		// チェック
+		if (rankdata[0].cursole_Y > MOJIBAN_MASUMAX_Y)
+		{
+			rankdata[0].cursole_Y = 0;
+		}
+	}
+	else if (GetKeyboardTrigger(DIK_D))
+	{
+		// 右キー
+		rankdata[0].cursole_X++;
+		// チェック
+		if (rankdata[0].cursole_X >MOJIBAN_MASUMAX_X)
+		{
+			rankdata[0].cursole_X = 0;
+		}
+	}
+	else if (GetKeyboardTrigger(DIK_A))
+	{
+		// 左キー
+		rankdata[0].cursole_X--;
+		// チェック
+		if (rankdata[0].cursole_X < 0)
+		{
+			rankdata[0].cursole_X = MOJIBAN_MASUMAX_X;
+		}
+	}
+
+	// 決定キーが入力された場合今のカーソル位置の文字を格納
+	if (GetKeyboardTrigger(DIK_SPACE))
+	{	// 数字で文字を格納
+		rankdata[0].namechar[namechar] = rankdata[0].cursole_X + (rankdata[0].cursole_Y * 10);
+		namechar++;
+		// テクスチャのセット
+		select_moji[namechar].Init(SENTAKUMOJI_POS_X, SENTAKUMOJI_POS_Y, SENTAKUMOJI_WIDTH, SENTAKUMOJI_HEIGHT, SENTAKUMOJI_TEX);
+		select_moji[namechar].SetTexture(rankdata[0].namechar[namechar], 10, 10);	//第二引数 ますめの数X 第3 ますめの数Y
+		// 空白文字の場合の処理(空白文字入力できるかわからないので保留)
+	}
+
+	// 文字入力の終了
+	if (GetKeyboardTrigger(DIK_Z))
+	{
+		int player_score = GetScore();
+		// あたまがはたらかなかった↓
+		long long name_number = (rankdata[0].namechar[4] * 100000000) + (rankdata[0].namechar[3] * 1000000) + (rankdata[0].namechar[2] * 10000) + (rankdata[0].namechar[1] * 100) + rankdata[0].namechar[0];
+		SaveRanking(player_score,name_number);
+	}
+
+	// 削除キーが入力された場合1文字戻る
+	if (GetKeyboardTrigger(DIK_BACKSPACE))
+	{
+		rankdata[0].namechar[namechar] = EMPTY_NO;
+		namechar--;
+	}
+	// 00があ　01がい
+	//mojiban.SetTexture(1,10,10);
 }
 
 
-
+//***************************************************************************************
+// void DrawChar(long long char)
+// 画像の一部分を切り取って表示する
+//***************************************************************************************
--- name.h
+++ name.h
@@ -6,20 +6,10 @@
 #define _NAME_H_
 
 #include "Library/Input.h"
+
+
 // でふぁいん
 #define NAMEMAX	(5)
-// もじばんだ
-#define MOJIBAN_TEX		("data/TEXTURE/mojiban.png")
-#define MOJIBAN_POS_X	(0.0)
-#define MOJIBAN_POS_Y	(0.0)
-#define MOJIBAN_WIDTH	(50.0)
-#define MOJIBAN_HEIGHT	(100.0)
-// カーソル
-#define CURSOLE_TEX		("data/TEXTURE/カーソル")
-#define CURSOLE_POS_X	(0.0)
-#define CURSOLE_POS_Y	(0.0)
-#define CURSOLE_WIDTH	(10.0)
-#define CURSOLE_HEIGHT	(10.0)
 
 /*****************************************************************************
 どさいどん
@@ -30,18 +20,17 @@ typedef struct
 {
 	char name[256];	// ユーザー名
 	int score;		// スコア
-	int namechar[NAMEMAX];
-	int cursole_X;
-	int cursole_Y;
+	long long namechar[NAMEMAX];
+	long long  cursole_X;
+	long long  cursole_Y;
 }RANKDATA;
 
 
 void Update_Name(void);
-int get_char(void);
-void InitName(void);
+HRESULT InitName(void);
+void UninitName(void);
+void DrawName(void);
 
 
-
-
-// 仮の処理
 #endif
+
